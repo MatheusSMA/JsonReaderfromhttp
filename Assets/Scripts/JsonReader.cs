1155518ff40d127d7737a400bdcb540a25084a1d
Matheus Simões <matheusjut@gmail.com>
2021-08-30 23:28:56 -0300

    initial changes on unity

--- Assets/Scripts/JsonReader.cs
+++ Assets/Scripts/JsonReader.cs
@@ -1,9 +1,8 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class JsonReader : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
